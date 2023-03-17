78821c9283a993d0908f881f27ff8b32fe331cc3
AYOMIDE <gaelegbede@student.oauife.edu.ng>
2023-03-16 17:52:00 -0700

    m

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+        //string initialisation
+    char Mystr[] = "\"Programming is like building a \"";
+
+    puts(Mystr); //writing the string to stdout
+
+    return 0;
+}
