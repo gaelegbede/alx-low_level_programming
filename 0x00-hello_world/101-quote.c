7c851583da308eec1f1bbe5a26cdb101a2046f3f
AYOMIDE <gaelegbede@student.oauife.edu.ng>
2023-03-16 20:15:35 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 # include <unistd.h>
 #include <sys/syscall.h>
+/**
+ * main - Entry point
+ *Return: Always 0 (Success)
+*/
 int main ()
 {
 	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
