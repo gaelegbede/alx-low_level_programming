1c52d8895c5553e3591b0353b4c0e848da3fcbb2
AYOMIDE <gaelegbede@student.oauife.edu.ng>
2023-03-16 20:17:25 -0700

    happy

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <sys/syscall.h>
 /**
  * main - Entry point
- *Return: Always 0 (Success)
+ *Return: Always 1 (Success)
 */
 int main ()
 {
 	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	syscall(SYS_write, 1, msg, sizeof(msg));
-	return 0;
+	return 1;
 }
