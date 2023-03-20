99b0afdca655aaf8a86f098284df8d2a776f097b
AYOMIDE <gaelegbede@student.oauife.edu.ng>
2023-03-19 21:19:19 -0700

    m

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,12 +4,12 @@
 #include <stdlib.h>
 /**
  * main- entry point
- * Return = Always (0) Success
+ * Return: Always (0) Success
  */
 int main(void)
 {
 	int c;
-	for (c=48; c<= 48+9; c++)
+	for (c = 48; c <= 48+9; c++)
 
 	{
 		putchar (c);
