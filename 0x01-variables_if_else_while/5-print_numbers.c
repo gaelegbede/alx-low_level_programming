a5607e15573192fec11caab2d5cc149245ea2901
AYOMIDE <gaelegbede@student.oauife.edu.ng>
2023-03-19 20:17:38 -0700

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main = Entry point
+ */
+int main(void)
+{
+	int w;
+	for (w = 0; w < 10; w++)
+{
+	printf("%d", w);
+}
+{
+	putchar ('\n');
+}
+return (0);
+}
