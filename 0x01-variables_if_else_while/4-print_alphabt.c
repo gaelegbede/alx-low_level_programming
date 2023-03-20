0b01cd2e3ab321799d3004a99d150ee25aa2ab51
AYOMIDE <gaelegbede@student.oauife.edu.ng>
2023-03-19 19:50:13 -0700

    alx

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,14 +6,15 @@
 	 * main - Entry point
 	 * Return: Always 0 Success
 	 */
-	int main ()
+	int main(void)
 {
 	char low, e, q;
-	e= 'e';
-	q= 'q';
-	for (low ='a'; low<= 'z'; low++)
+	e = 'e';
+
+	q = 'q';
+	for (low ='a'; low <= 'z'; low++)
 	{
-		if (low !=e && low!= q)
+		if (low != e && low != q)
 		putchar (low);
 	}
 putchar ('\n');
