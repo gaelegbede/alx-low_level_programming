5393c9a287e315dd0f950ba97ec431d9e1ed27f7
AYOMIDE <gaelegbede@student.oauife.edu.ng>
2023-03-20 16:39:55 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 */
 int main ()
 {
-	const char msg[] ="and that piece of art is useful\n"- Dora Korpar, 2015-10-19";
+	const char msg[] =""and that piece of art is useful\n"- Dora Korpar, 2015-10-19";
 	syscall(SYS_write, 1, msg, sizeof(msg));
-	return 1;
+	return (1);
 }
