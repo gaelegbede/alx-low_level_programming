47a0d8cabe12c42be87f93d4fab5e9b62d8eb5b8
AYOMIDE <gaelegbede@student.oauife.edu.ng>
2023-03-19 21:09:30 -0700

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main- entry point
+ * Return = (0) Success
+ */
+int main ()
+{
+	int c;
+	for (c=48; c<= 48+9; c++)
+
+	{
+		putchar (c);
+	}
+	{
+		putchar ('\n');
+	}
+	return (0);
+}
