7640a0b03882e7a47fd845e8bee4e40fe23a81db
AYOMIDE <gaelegbede@student.oauife.edu.ng>
2023-03-20 16:23:18 -0700

    m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main()
 	char charType;
 	long long int longltype;
 	sizeof evaluates the size of a variable
-		printf("Size of char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
