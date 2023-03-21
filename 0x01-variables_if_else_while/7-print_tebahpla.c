fe0f255e2be38e19c00edb8b4da43ed67eef2f4a
AYOMIDE <gaelegbede@student.oauife.edu.ng>
2023-03-20 17:33:41 -0700

    m

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void)
+{
+	char i;
+	for (i='z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+{
+	putchar('\n');
+}
+    return 0;
+}
