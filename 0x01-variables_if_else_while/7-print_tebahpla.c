fe5da7985ae5703d02bf6dadb2be4ff53c56b4f2
AYOMIDE <gaelegbede@student.oauife.edu.ng>
2023-03-20 18:19:50 -0700

    m

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * main - Entry point
+ * Return: always 0 success
+ */
 int main(void)
 {
 	char i;
-	for (i='z'; i >= 'a'; i--)
+	
+	for (i = 'z'; i >= 'a'; i--)
 	{
 		putchar(i);
 	}
 {
 	putchar('\n');
 }
-    return 0;
+   return (0);
 }
