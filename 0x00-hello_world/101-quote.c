0772b9e40a47eb84e9e488d01ac1091e40be3b54
AYOMIDE <gaelegbede@student.oauife.edu.ng>
2023-03-20 17:16:13 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,8 @@
  * main - Entry point
  *Return: Always 1 (Success)
 */
-int main ()
+int main (void)
 {
-	const char msg[] =""and that piece of art is useful\n"- Dora Korpar, 2015-10-19";
-	syscall(SYS_write, 1, msg, sizeof(msg));
+	write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
